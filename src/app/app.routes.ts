64bb45a466d7ffdb593f56c7eb95d3e01c94c158
Murilo Aparecido Carvalho Nascimento <murilo.aparecido@ufms.br>
2025-03-03 13:06:49 -0300

    Ajuste das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,12 @@ import { authGuard } from './guards/auth.guard';
 import { UsuariosListComponent } from './pages/usuarios/usuarios-list/usuarios-list.component';
 import { UsuariosCreateComponent } from './pages/usuarios/usuarios-create/usuarios-create.component';
 import { UsuariosInativosComponent } from './pages/usuarios-inativos/usuarios-inativos.component';
+import { ContatosListComponent } from './pages/contatos/contatos-list/contatos-list.component';
+import { ContatosCreateComponent } from './pages/contatos/contatos-create/contatos-create.component';
+import { ContatosInativosComponent } from './pages/contatos-inativos/contatos-inativos.component';
+import { TiposListComponent } from './pages/tipos/tipos-list/tipos-list.component';
+import { TiposCreateComponent } from './pages/tipos/tipos-create/tipos-create.component';
+import { TiposInativosComponent } from './pages/tipos-inativos/tipos-inativos.component';
 
 export const routes: Routes = [
   {
@@ -31,24 +37,24 @@ export const routes: Routes = [
       {
         path: 'contatos',
         children: [
-          { path: '', component: UsuariosListComponent },
-          { path: 'create', component: UsuariosCreateComponent },
+          { path: '', component: ContatosListComponent },
+          { path: 'create', component: ContatosCreateComponent },
         ],
       },
       {
         path: 'contatos-inativos',
-        component: UsuariosInativosComponent,
+        component: ContatosInativosComponent,
       },
       {
         path: 'tipos',
         children: [
-          { path: '', component: UsuariosListComponent },
-          { path: 'create', component: UsuariosCreateComponent },
+          { path: '', component: TiposListComponent },
+          { path: 'create', component: TiposCreateComponent },
         ],
       },
       {
         path: 'tipos-inativos',
-        component: UsuariosInativosComponent,
+        component: TiposInativosComponent,
       },
     ],
   },
